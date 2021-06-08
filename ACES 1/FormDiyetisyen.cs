718f9d850c65a3f7f2b87f92216f8cfe6dd7c28b
Enes Ertuğrul Koyuncu <enes.koyuncu5507@gmail.com>
2021-06-08 15:27:11 +0300

    e7
    FormDiyetisyen üzerinde duruldu.
    Uml diagramında değişikliğe gidildi.
    FormMüşteri ve FormAdmin oluşturulacak.

--- ACES 1/FormDiyetisyen.cs
+++ ACES 1/FormDiyetisyen.cs
@@ -25,7 +25,47 @@ namespace ACES_1
 
         }
 
+        private void FormDiyetisyen_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'diyetveri_050621DataSet.ogun_tip' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.ogun_tipTableAdapter.Fill(this.diyetveri_050621DataSet.ogun_tip);
+
+            Data data1 = new Data();
+            MySqlCommand command = new MySqlCommand();
+            command = data1.getCommand("select ogunTip from ogun_tip ");
+            MySqlDataReader read = command.ExecuteReader();
+
+            try
+            {
+                while(read.Read())
+                {
+                    cmbOgunTip.Items.Add(read[0].ToString());
+
+                }
+                
+            }
+            catch(Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
 
 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            DataGridViewRow selectedRow = dataGridView1.Rows[index];
+            lblMusteriAd.Text = selectedRow.Cells[0].Value.ToString();
+            lblMusteriSoyad.Text = selectedRow.Cells[1].Value.ToString();
+            lblMusteriHastalik.Text = selectedRow.Cells[2].Value.ToString();
+            cmbOgunTip.Text = selectedRow.Cells[3].Value.ToString();
+
+        }
     }
 }
