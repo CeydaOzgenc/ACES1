5c11c661cf22829fbdc220245952cb788d896b1f
Enes Ertuğrul Koyuncu <enes.koyuncu5507@gmail.com>
2021-06-09 01:34:32 +0300

    e8
    FormAdmin halledildi.

--- ACES 1/Program.cs
+++ ACES 1/Program.cs
@@ -16,7 +16,7 @@ namespace ACES_1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormDiyetisyen());
+            Application.Run(new FormAdmin());
         }
     }
 }
