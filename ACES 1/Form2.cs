2d6cfe329ce3a5c6dba36c655cd4893be9c2e702
Enes Ertuğrul Koyuncu <enes.koyuncu5507@gmail.com>
2021-06-08 12:50:01 +0300

    v5

--- ACES 1/Form2.cs
+++ ACES 1/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ACES_1
 {
@@ -15,6 +16,16 @@ namespace ACES_1
         public Form2()
         {
             InitializeComponent();
+            Data data1 = new Data();
+            MySqlDataAdapter adtr = new MySqlDataAdapter();
+            adtr = data1.getAdapter("select k.name,k.surname,k.hastalik, d.ogunTipID from kullanici k, diyet_takvim d where tip LIKE '%Müşteri%'AND k.hastalik = d.hastalik ");
+            DataTable dtbl1 = new DataTable();
+            adtr.Fill(dtbl1);
+            dataGridView1.DataSource = dtbl1;
+
         }
+
+
+
     }
 }
--- ACES 1/GirisForm.Designer.cs
+++ ACES 1/GirisForm.Designer.cs
@@ -35,8 +35,6 @@ namespace ACES_1
             this.txtSifre = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTC
@@ -94,31 +92,11 @@ namespace ACES_1
             this.button2.Text = "Kayıt Ol";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(83, 260);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(251, 260);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
-            // 
             // GirisForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(477, 347);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtSifre);
@@ -140,7 +118,5 @@ namespace ACES_1
         private System.Windows.Forms.TextBox txtSifre;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
