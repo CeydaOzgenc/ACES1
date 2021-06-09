9a22e63e5df0533058c0c024d37b5c8eec5c2d06
Enes Ertuğrul Koyuncu <enes.koyuncu5507@gmail.com>
2021-06-09 03:40:35 +0300

    e10
    Giriş sonrası gidilecek formları düzenledim ve hallettim. Tek tek formları kontrol edeceğim

---
+++ ACES 1/FormMüşteri.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ACES_1
+{
+    public partial class FormMüşteri : Form
+    {
+        public FormMüşteri(string TC)
+        {
+            InitializeComponent();
+        }
+
+        private void FormMüşteri_Load(object sender, EventArgs e)
+        {
+            MySqlCommand command = new MySqlCommand();
+            Data dt1 = new Data();
+            command = dt1.getCommand("select * from kullanici");
+
+
+
+
+        }
+    }
+}
